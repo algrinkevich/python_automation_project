1029bc8623dbfb4cb0d35c007f0056a155087e03
agrinkevich <grinkevich.alina@gmail.com>
2022-05-24 19:48:16 +0300

    login methods were moved to base_page

--- pages/product_page.py
+++ pages/product_page.py
@@ -5,7 +5,7 @@ from .base_page import BasePage
 class ProductPage(BasePage):
     def add_to_basket(self):
         self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BTN).click()
-        self.solve_quiz_and_get_code()
+        #self.solve_quiz_and_get_code()
 
     def should_be_added_to_basket(self):
         self.should_see_success_message()
@@ -37,7 +37,11 @@ class ProductPage(BasePage):
         product_cost = self.get_product_cost()
         assert product_cost in basket_total_message
 
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.PRODUCT_NAME_IN_SUCCESS_TOAST), \
+            "Success message is presented, but should not be"
 
-
-
+    def should_disappear_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.PRODUCT_NAME_IN_SUCCESS_TOAST), \
+            "The success message is still presented"
 
--- test_product_page.py
+++ test_product_page.py
@@ -2,6 +2,7 @@ import time
 import pytest
 
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 
 
 def generate_links():
@@ -10,10 +11,46 @@ def generate_links():
     return links
 
 
-@pytest.mark.parametrize('link', generate_links())
-def test_guest_can_add_product_to_basket(browser, link):
-    product_page = ProductPage(browser, link)
+#@pytest.mark.parametrize('link', generate_links())
+def test_guest_can_add_product_to_basket(browser):
+    product_page = ProductPage(browser, "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     product_page.open()
+    product_page.should_not_be_success_message()
     product_page.add_to_basket()
     product_page.should_be_added_to_basket()
 
+
+@pytest.mark.xfail(reason="the test was created intentionally failed")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    product_page = ProductPage(browser, "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    product_page.open()
+    product_page.add_to_basket()
+    product_page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    product_page = ProductPage(browser, "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    product_page.open()
+    product_page.should_not_be_success_message()
+
+
+@pytest.mark.xfail(reason="the test was created intentionally failed")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    product_page = ProductPage(browser, "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    product_page.open()
+    product_page.add_to_basket()
+    product_page.should_disappear_success_message()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    product_page = ProductPage(browser, "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/")
+    product_page.open()
+    product_page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    product_page = ProductPage(browser, "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/")
+    product_page.open()
+    product_page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
